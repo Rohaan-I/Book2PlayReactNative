0d378ac107adccd649cac0ac8ee7f2aaeac309d5
Rohaan Ishfaq <rohaan.ishfaq@telenor.com.pk>
2018-08-26 23:16:56 +0500

    signUpScreen.js is in progress.

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, Alert, ActivityIndicator, Keyboard } from 'react-native';
+import { View, StyleSheet, Text, Image, Alert, ActivityIndicator, Keyboard, Picker, ScrollView } from 'react-native';
 import { Card, Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
 import Auth from '../services/Auth';
@@ -18,6 +18,7 @@ export default class SignUpScreen extends React.Component {
             city: '',
             country: '',
             phone: '',
+            userRole: '1',
             hasEmail: true,
             hasPass: true,
             hasFullName: true,
@@ -31,9 +32,19 @@ export default class SignUpScreen extends React.Component {
         this._auth = new Auth();
     }
 
-    _doSignIn = async () => {
+    _doSignUp = async () => {
         
         try {
+            if(this.state.fullName == '') {
+                this.setState({
+                    hasFullName: false
+                })
+            } 
+            else {
+                this.setState({
+                    hasFullName: true
+                });
+            }
             if(this.state.email == '') {
                 this.setState({
                     hasEmail: false
@@ -54,15 +65,91 @@ export default class SignUpScreen extends React.Component {
                     hasPass: true
                 });
             }
+            if(this.state.streetAddress == '') {
+                this.setState({
+                    hasStreetAddress: false
+                })
+            } 
+            else {
+                this.setState({
+                    hasStreetAddress: true
+                });
+            }
+            if(this.state.city == '') {
+                this.setState({
+                    hasCity: false
+                })
+            } 
+            else {
+                this.setState({
+                    hasCity: true
+                });
+            }
+            if(this.state.country == '') {
+                this.setState({
+                    hasCountry: false
+                })
+            } 
+            else {
+                this.setState({
+                    hasCountry: true
+                });
+            }
+            if(this.state.phone == '') {
+                this.setState({
+                    hasPhone: false
+                })
+            } 
+            else {
+                this.setState({
+                    hasPhone: true
+                });
+            }
 
-            if(this.state.email && this.state.password) {
+            if(this.state.fullName && this.state.email && this.state.password && this.state.streetAddress
+                && this.state.city && this.state.country && this.state.phone) {
 
                 Keyboard.dismiss();
                 this.setState({
                     loading: true
                 });
 
-                let response = await this._auth.signInUser(this.state.email.trim(), this.state.password.trim());
+                /*{
+                    "name": "Rohaan Ishfaq",
+                    "email": "rohaanishfaq@gmail.com",
+                    "password": "ronio658",
+                    "address": {
+                        "streetAddress": "test 123",
+                        "poBox": "test 123",
+                        "city": "5accce61cf4d6a334ce779d7",
+                        "country": "5accce2c19d1bb3d7847a193"
+                    },
+                    "contactNumber": "03219579116",
+                    "role": "Field Manager",
+                    "location": {
+                        "lat": 25.2048493,
+                        "lng": 55.2707828
+                    }
+                }*/
+
+                let reqObject = {
+                    name: this.state.fullName.trim(),
+                    email: this.state.email.trim(), 
+                    password: this.state.password.trim(),
+                    address: {
+                        streetAddress: this.state.streetAddress.trim(),
+                        poBox: this.state.poBox.trim(),
+                        city: this.state.city.trim(),
+                        country: this.state.country.trim()
+                    },
+                    contactNumber: this.state.phone.trim(),
+                    role: this.state.userRole,
+                    location: {
+                        lat: 25.2048493,
+                        lng: 55.2707828
+                    }   
+                }
+                let response = await this._auth.signUpUser(reqObject);
                 console.log(response);
 
                 this.setState({
@@ -80,7 +167,7 @@ export default class SignUpScreen extends React.Component {
                       );
                 }
                 else {
-                    // storing user role and login flag in async storage.
+                    this.props.navigation.navigate('SignIn');
                 }
             }
         }
@@ -116,39 +203,61 @@ export default class SignUpScreen extends React.Component {
 
     render() {            
         return (
-            <View style={styles.container}>
-                <Image
-                    style={styles.logo}
-                    source={require('../assets/images/logo_field.png')}
-                />
-                <View>
-                    <FormInput autoCapitalize='none' placeholder='Full Name*' inputStyle={styles.formInput} onChangeText={(fullName) => {this.setState({ fullName });} } />
-                    {!this.state.hasFullName ? <FormValidationMessage>{'Full Name is required'}</FormValidationMessage> : null } 
-                    <FormInput autoCapitalize='none' placeholder='Email*' inputStyle={styles.formInput} onChangeText={(email) => {this.setState({ email });} } />
-                    {!this.state.hasEmail ? <FormValidationMessage>{'Email is required'}</FormValidationMessage> : null } 
-                    <FormInput autoCapitalize='none' secureTextEntry placeholder='Password*' inputStyle={styles.formInput} onChangeText={(password) => this.setState({password})} />
-                    {!this.state.hasPass ? <FormValidationMessage>{'Password is required'}</FormValidationMessage> : null } 
-                    <FormInput autoCapitalize='none' placeholder='Street Address*' inputStyle={styles.formInput} onChangeText={(streetAddress) => {this.setState({ streetAddress });} } />
-                    {!this.state.hasStreetAddress ? <FormValidationMessage>{'Street Address is required'}</FormValidationMessage> : null } 
-                    <FormInput autoCapitalize='none' placeholder='P.O. Box' inputStyle={styles.formInput} onChangeText={(poBox) => {this.setState({ poBox });} } />
-                    <FormInput autoCapitalize='none' placeholder='City*' inputStyle={styles.formInput} onChangeText={(city) => {this.setState({ city });} } />
-                    {!this.state.hasCity ? <FormValidationMessage>{'City is required'}</FormValidationMessage> : null } 
-                    <FormInput autoCapitalize='none' placeholder='Country*' inputStyle={styles.formInput} onChangeText={(country) => {this.setState({ country });} } />
-                    {!this.state.hasCountry ? <FormValidationMessage>{'Country is required'}</FormValidationMessage> : null } 
-                    <FormInput autoCapitalize='none' placeholder='Phone/Mobile*' inputStyle={styles.formInput} onChangeText={(phone) => {this.setState({ phone });} } />
-                    {!this.state.hasPhone ? <FormValidationMessage>{'Phone/Mobile is required'}</FormValidationMessage> : null } 
-                    
-                    <Button onPress={this._doSignIn}  buttonStyle={styles.button} color='#052c52' fontWeight='bold' title='Create An Account' />
-                </View>
-                
-                { this.state.loading ?
-                    
-                    <View pointerEvents='none' style={styles.loading}>
-                        <ActivityIndicator size="large" color="#052c52" />
-                    </View>  
+            <ScrollView contentContainerStyle={styles.contentContainer}>
+                <View style={styles.container}>
+                    <Image
+                        style={styles.logo}
+                        source={require('../assets/images/logo_field.png')}
+                    />
+                    <View>
+                        <FormInput autoCapitalize='none' placeholder='Full Name*' inputStyle={styles.formInput} onChangeText={(fullName) => {this.setState({ fullName });} } />
+                        {!this.state.hasFullName ? <FormValidationMessage>{'Full Name is required'}</FormValidationMessage> : null } 
+                        <FormInput autoCapitalize='none' placeholder='Email*' inputStyle={styles.formInput} onChangeText={(email) => {this.setState({ email });} } />
+                        {!this.state.hasEmail ? <FormValidationMessage>{'Email is required'}</FormValidationMessage> : null } 
+                        <FormInput autoCapitalize='none' secureTextEntry placeholder='Password*' inputStyle={styles.formInput} onChangeText={(password) => this.setState({password})} />
+                        {!this.state.hasPass ? <FormValidationMessage>{'Password is required'}</FormValidationMessage> : null } 
+                        <FormInput autoCapitalize='none' placeholder='Street Address*' inputStyle={styles.formInput} onChangeText={(streetAddress) => {this.setState({ streetAddress });} } />
+                        {!this.state.hasStreetAddress ? <FormValidationMessage>{'Street Address is required'}</FormValidationMessage> : null } 
+                        <FormInput autoCapitalize='none' placeholder='P.O. Box' inputStyle={styles.formInput} onChangeText={(poBox) => {this.setState({ poBox });} } />
+                        
+                        <FormInput autoCapitalize='none' placeholder='City*' inputStyle={styles.formInput} onChangeText={(city) => {this.setState({ city });} } />
+                        {/* <Picker
+                            selectedValue={this.state.city}
+                            style={styles.userRole}
+                            onValueChange={(itemValue, itemIndex) => this.setState({city: itemValue})}>
+                            <Picker.Item label="List my field" value="1" />
+                            <Picker.Item label="Reserve a field" value="2" />
+                        </Picker> */}
+                        {!this.state.hasCity ? <FormValidationMessage>{'City is required'}</FormValidationMessage> : null } 
+                        
+                        <FormInput autoCapitalize='none' placeholder='Country*' inputStyle={styles.formInput} onChangeText={(country) => {this.setState({ country });} } />
+                        {!this.state.hasCountry ? <FormValidationMessage>{'Country is required'}</FormValidationMessage> : null } 
+                        <FormInput autoCapitalize='none' placeholder='Phone/Mobile*' inputStyle={styles.formInput} onChangeText={(phone) => {this.setState({ phone });} } />
+                        {!this.state.hasPhone ? <FormValidationMessage>{'Phone/Mobile is required'}</FormValidationMessage> : null } 
+                        
+                        <Picker
+                            selectedValue={this.state.userRole}
+                            style={styles.userRole}
+                            onValueChange={(itemValue, itemIndex) => this.setState({userRole: itemValue})}>
+                            <Picker.Item label="List my field" value="1" />
+                            <Picker.Item label="Reserve a field" value="2" />
+                        </Picker>
 
-                : null}
-            </View>
+                        <Button onPress={this._doSignUp}  buttonStyle={styles.button} color='#052c52' fontWeight='bold' title='Create An Account' />
+                        <Button buttonStyle={styles.signInButton} fontWeight='bold' title='Sign In' onPress={() => this.props.navigation.navigate('SignIn') }/> 
+                        
+                        <Text style={styles.terms}>By creating an account, you accept and agree to our <Text style={{textDecorationLine: 'underline'}} onPress={this._goToTermsScreen}>Terms of Use</Text></Text>
+                    </View>
+                    
+                    { this.state.loading ?
+                        
+                        <View pointerEvents='none' style={styles.loading}>
+                            <ActivityIndicator size="large" color="#052c52" />
+                        </View>  
+
+                    : null}
+                </View>
+            </ScrollView>
         );
     }
 }
@@ -166,7 +275,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
         backgroundColor: '#efb225'
     },
-    signUpButton: {
+    signInButton: {
         marginTop: 10,
         backgroundColor: '#052c52'
     },
@@ -175,10 +284,6 @@ const styles = StyleSheet.create({
         height: 150, 
         marginTop: 10,
         resizeMode:'contain'
-    },
-    forgotPassBtn: {
-        textDecorationLine: 'underline', 
-        marginTop: 20
     },
     loading: {
         position: 'absolute',
@@ -190,17 +295,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: '#f5fcff88'
     },
-    policy: {
-        flex:1, 
-        flexDirection: 'column', 
-        justifyContent: 'center', 
-        alignItems: 'center'
+    terms: { 
+        marginLeft: 15, 
+        marginTop: 10 
     },
-    termsBtn: {
-        textDecorationLine: 'underline'
+    contentContainer: {
+        paddingVertical: 20
     },
-    privacyBtn: {
-        textDecorationLine: 'underline', 
-        marginTop: 10
+    userRole: { 
+        width: 350, 
+        marginLeft: 10 
     }
 });
--- services/Auth.js
+++ services/Auth.js
@@ -4,9 +4,11 @@ import Base from '../services/Base';
 export default class Auth extends Base {
 
     _authUrl = '';
+    _signUpUrl = '';
     constructor() {
         super();
         this._authUrl = this.getBaseUrl() + 'account/authenticate';
+        this._signUpUrl = this.getBaseUrl() + 'account';
     }
 
     async signInUser(email, password) {
@@ -22,6 +24,26 @@ export default class Auth extends Base {
                     email: email,
                     password: password,
                 }),
+            });
+
+            let responseJSON = response.json();
+            return responseJSON;
+        }
+        catch(err) {
+            throw new Error(err);
+        }
+    }
+
+    async signUpUser(reqObject) {
+        try {
+            
+            let response = await fetch(this._signUpUrl, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(reqObject),
             });
 
             let responseJSON = response.json();
